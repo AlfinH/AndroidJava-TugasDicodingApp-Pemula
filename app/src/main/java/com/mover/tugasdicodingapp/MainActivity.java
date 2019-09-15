b62e552288e78d618915f6e0bcb8531a551ad1c1
alfin-jq <alfinherianto8@gmail.com>
2019-09-15 13:08:25 +0700

    Initial commit

---
+++ app/src/main/java/com/mover/tugasdicodingapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.mover.tugasdicodingapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.dicoding_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.user_detail:
+//                saveHalte();
+                return true;
+            //add more item if you have another item to be clicked
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
